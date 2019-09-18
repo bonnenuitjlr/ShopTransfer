135173975d4ebbf2dd968beb0474bb0bd2aed4c8
yangxingyu <1060607327@qq.com>
2019-09-18 14:30:32 +0800

    搜索功能

--- src/main/java/com/xinyu/ShopTransfer/controller/ShopDataController.java
+++ src/main/java/com/xinyu/ShopTransfer/controller/ShopDataController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.Resource;
 
+import com.sun.scenario.effect.impl.hw.ShaderSource;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -19,27 +20,44 @@ import com.xinyu.ShopTransfer.util.Response;
 import com.xinyu.ShopTransfer.vo.ShopVo;
 
 @RestController
-@RequestMapping(value="shop")
+@RequestMapping(value = "shop")
 public class ShopDataController {
-	
-	private static Log log = LogFactory.getLog(ShopController.class);
-	
-	@Resource
+
+    private static Log log = LogFactory.getLog(ShopController.class);
+
+    @Resource
     private ShopService shopService;
-	
-	@RequestMapping(value="data1", method = { RequestMethod.GET })			// 测试使用
-    public Response data1(@RequestParam("page")Integer page,@RequestParam("limit")Integer limit){
-		List<ShopBean> allList = shopService.getAllMessage1();
-    	List<ShopBean> pageList = shopService.getConditionMessage1(page,limit);
-    	log.info("list size="+allList.size()+"  current list info="+GsonUtil.toJson(pageList));
+
+    @RequestMapping(value = "data1", method = {RequestMethod.GET})            // 测试使用
+    public Response data1(@RequestParam("page") Integer page, @RequestParam("limit") Integer limit) {
+        List<ShopBean> allList = shopService.getAllMessage1();
+        List<ShopBean> pageList = shopService.getConditionMessage1(page, limit);
+        log.info("list size=" + allList.size() + "  current list info=" + GsonUtil.toJson(pageList));
         return Response.response(0, allList.size(), "OK", pageList);
     }
-	
-	@RequestMapping(value="data", method = { RequestMethod.POST })			// 测试使用
-    public Response data(@RequestBody ShopVo shopVo,@RequestParam("page")Integer page,@RequestParam("limit")Integer limit){
-		List<ShopBean> conditionList = shopService.getConditionMessage(shopVo);
-    	List<ShopBean> pageList = shopService.getPageConditionMessage(shopVo,page,limit);
-    	log.info("list size="+conditionList.size()+"  current list info="+GsonUtil.toJson(pageList));
+
+    @RequestMapping(value = "data", method = {RequestMethod.GET})            // 测试使用
+    public Response data(String title,
+                         String href,
+                         String region,
+                         Integer areaS,
+                         Integer areaE,
+                         String type,
+                         Integer priceS,
+                         Integer priceE,
+                         @RequestParam("page") Integer page, @RequestParam("limit") Integer limit) {
+        ShopVo shopVo = new ShopVo();
+        shopVo.setTitle(title);
+        shopVo.setHref(href);
+        shopVo.setRegion(region);
+        shopVo.setAreaS(areaS);
+        shopVo.setAreaE(areaE);
+        shopVo.setType(type);
+        shopVo.setPriceS(priceS);
+        shopVo.setPriceE(priceE);
+        List<ShopBean> conditionList = shopService.getConditionMessage(shopVo);
+        List<ShopBean> pageList = shopService.getPageConditionMessage(shopVo, page, limit);
+        log.info("list size=" + conditionList.size() + "  current list info=" + GsonUtil.toJson(pageList));
         return Response.response(0, conditionList.size(), "OK", pageList);
     }
 
